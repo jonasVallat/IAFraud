b8777ab841e8b7daf28e68c36c5fbc6529be0045
Vallat Jonas <jonas.vallat@he-arc.ch>
2020-04-26 20:01:40 +0200

    removed useless parts

--- TP4_IA.py
+++ TP4_IA.py
@@ -2,91 +2,48 @@ import pandas
 import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sn
+
 import sklearn
-
-data = pandas.read_csv('creditcard.csv')
-print(data.head())
-
-print(data.isna().any())
-
-corrwith = data.corrwith(data.Class).plot.bar(figsize = (20, 10), title = "Correlation with class", fontsize = 15, rot = 45, grid = True)
-#plt.subplot(corrwith)
-#plt.show()
-
-corr = data.corr()
-
-# Generate a mask for the upper triangle
-mask = np.zeros_like(corr, dtype=np.bool)
-mask[np.triu_indices_from(mask)] = True
-# Set up the matplotlib figure
-f, ax = plt.subplots(figsize=(18, 15))
-# Generate a custom diverging colormap
-cmap = sn.diverging_palette(220, 10, as_cmap=True)
-# Draw the heatmap with the mask and correct aspect ratio
-snHeatmap=sn.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0, square=True, linewidths=.5, cbar_kws={"shrink": .5})
-
-#plt.subplot(snHeatmap)
-#plt.show()
-
 from sklearn.preprocessing import StandardScaler
-
-data['normalizedAmount'] = StandardScaler().fit_transform(data['Amount'].values.reshape(-1,1))
-data = data.drop(['Amount'],axis=1)
-data = data.drop(['Time'],axis=1)
-data.head()
-
-#print(data.head())
-
-X = data.iloc[:, data.columns != 'Class']
-y = data.iloc[:, data.columns == 'Class']
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.3, random_state=0)
+from sklearn.metrics import classification_report
 
-# Importing the Keras libraries and packages
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
-# Initialising the ANN
-classifier = Sequential()
-# Adding the input layer and the first hidden layer
+
+data = pandas.read_csv('creditcard.csv')    #extracting data
+
+data = data.drop(['Time'],axis=1)   #removes irrelevent data
+
+x = data.iloc[:, data.columns != 'Class']   #split x/y data
+y = data.iloc[:, data.columns == 'Class']
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3, random_state=0)  #split test/training data
+
+classifier = Sequential()   #Initialising neural network
+
+#adding layers
 classifier.add(Dense(units =15 , kernel_initializer = 'uniform', activation = 'relu', input_dim = 29))
-# Adding the second hidden layer
 classifier.add(Dense(units = 15, kernel_initializer = 'uniform', activation = 'relu'))
-# Adding the output layer
 classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
-# Compiling the ANN
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-# Fitting the ANN to the Training set
-classifier.fit(X_train, y_train, batch_size = 32, epochs = 100)
 
-# Predicting the Test set results
-y_pred = classifier.predict(X_test)
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])    #compiling the neural network
+
+classifier.fit(x_train, y_train, batch_size = 32, epochs = 2)   #training set
+
+# Predicting the results
+y_pred = classifier.predict(x_test)
 y_pred = (y_pred > 0.5)
-score = classifier.evaluate(X_test, y_test)
-print("SCORE :", score)
 
-#Let's see how our model performed
-from sklearn.metrics import classification_report
+#Viewing performances
 print(classification_report(y_test, y_pred))
 
-## EXTRA: Confusion Matrix
-cm = sklearn.metrics.confusion_matrix(y_test, y_pred) # rows = truth, cols = prediction
+#Confusion Matrix
+cm = sklearn.metrics.confusion_matrix(y_test, y_pred)
 df_cm = pandas.DataFrame(cm, index = (0, 1), columns = (0, 1))
 plt.figure(figsize = (10,7))
 sn.set(font_scale=1.4)
 sn.heatmap(df_cm, annot=True, fmt='g')
-print("Test Data Accuracy: %0.4f" % sklearn.metrics.accuracy_score(y_test, y_pred))
+print("Accuracy: %0.4f" % sklearn.metrics.accuracy_score(y_test, y_pred))
 
-fpr, tpr, thresholds = sklearn.metrics.roc_curve(error_df.true_class, error_df.reconstruction_error)
-roc_auc = sklearn.metrics.auc(fpr, tpr)
-plt.title('Receiver Operating Characteristic')
-plt.plot(fpr, tpr, label='AUC = %0.4f'% roc_auc)
-plt.legend(loc='lower right')
-plt.plot([0,1],[0,1],'r--')
-plt.xlim([-0.001, 1])
-plt.ylim([0, 1.001])
-plt.ylabel('True Positive Rate')
-plt.xlabel('False Positive Rate')
 plt.show()
-
-print("end")
