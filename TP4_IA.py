36caf256a19c9eb1ec0adeb37a438ed5679b06b6
John Leuba <john.leuba@outlook.com>
2020-04-30 17:09:54 +0200

    Add roc curve

--- TP4_IA.py
+++ TP4_IA.py
@@ -9,6 +9,7 @@ import sklearn # Machine-learning library
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split # Split data into train and test subse
 from sklearn.metrics import classification_report # Printt the main metrics
+from sklearn.metrics import roc_auc_score, roc_curve
 
 import keras
 from keras.models import Sequential
@@ -37,10 +38,13 @@ classifier.fit(x_train, y_train, batch_size = 32, epochs = 2) # Training set
 y_pred = classifier.predict(x_test)
 y_pred = (y_pred > 0.5)
 
-#Viewing performances
+# Predict positive results probabilities
+y_prob = classifier.predict_proba(x_test)  
+
+# Viewing performances
 print(classification_report(y_test, y_pred))
 
-#Confusion Matrix
+# Confusion Matrix
 cm = sklearn.metrics.confusion_matrix(y_test, y_pred)
 df_cm = pandas.DataFrame(cm, index = (0, 1), columns = (0, 1))
 plt.figure(figsize = (10,7)) # Prepare display of figure
@@ -48,4 +52,30 @@ sn.set(font_scale=1.4)
 sn.heatmap(df_cm, annot=True, fmt='g') # Set the figure data with the confusion matrix data frame
 print("Accuracy: %0.4f" % sklearn.metrics.accuracy_score(y_test, y_pred))
 
+plt.show()
+
+
+# ROC Curve
+probs_no_skill = [0 for _ in range(len(y_test))]
+
+auc_no_skill = roc_auc_score(y_test, probs_no_skill)
+auc_y = roc_auc_score(y_test, y_prob)
+
+# Calculate roc curves
+fpr_no_skill, tpr_no_skill, _ = roc_curve(y_test, probs_no_skill)
+y_fpr, y_tpr, _ = roc_curve(y_test, y_prob)
+# Plot the roc curve for the model
+plt.plot(fpr_no_skill, tpr_no_skill, linestyle='--', label='No Skill')
+plt.plot(y_fpr, y_tpr, marker='.', label='Predicted')
+# Axis labels
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+# Show the legend
+plt.legend()
+
+# Print the AUC scores
+print('No Skill: ROC AUC=%.3f' % (auc_no_skill))
+print('Logistic: ROC AUC=%.3f' % (auc_y))
+
+# Show the plot
 plt.show()
